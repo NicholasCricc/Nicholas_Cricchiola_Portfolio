e97baf5f3848ed3a0b1903648597154c073047a0
NicholasCricc <56021244+NicholasCricc@users.noreply.github.com>
2024-08-23 18:47:25 +0200

    commit

--- ToonTanks/Intermediate/Build/Win64/UnrealEditor/Inc/ToonTanks/UHT/Tank.gen.cpp
+++ ToonTanks/Intermediate/Build/Win64/UnrealEditor/Inc/ToonTanks/UHT/Tank.gen.cpp
@@ -102,14 +102,14 @@ ATank::~ATank() {}
 // End Class ATank
 
 // Begin Registration
-struct Z_CompiledInDeferFile_FID_ToonTanks_Source_ToonTanks_Tank_h_Statics
+struct Z_CompiledInDeferFile_FID_Users_nicho_OneDrive_portofolio_Nicholas_Cricchiola_Portfolio_ToonTanks_Source_ToonTanks_Tank_h_Statics
 {
 	static constexpr FClassRegisterCompiledInInfo ClassInfo[] = {
 		{ Z_Construct_UClass_ATank, ATank::StaticClass, TEXT("ATank"), &Z_Registration_Info_UClass_ATank, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(ATank), 458155921U) },
 	};
 };
-static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_ToonTanks_Source_ToonTanks_Tank_h_3339209421(TEXT("/Script/ToonTanks"),
-	Z_CompiledInDeferFile_FID_ToonTanks_Source_ToonTanks_Tank_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_ToonTanks_Source_ToonTanks_Tank_h_Statics::ClassInfo),
+static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Users_nicho_OneDrive_portofolio_Nicholas_Cricchiola_Portfolio_ToonTanks_Source_ToonTanks_Tank_h_3339209421(TEXT("/Script/ToonTanks"),
+	Z_CompiledInDeferFile_FID_Users_nicho_OneDrive_portofolio_Nicholas_Cricchiola_Portfolio_ToonTanks_Source_ToonTanks_Tank_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_Users_nicho_OneDrive_portofolio_Nicholas_Cricchiola_Portfolio_ToonTanks_Source_ToonTanks_Tank_h_Statics::ClassInfo),
 	nullptr, 0,
 	nullptr, 0);
 // End Registration
--- ToonTanks/Intermediate/Build/Win64/UnrealEditor/Inc/ToonTanks/UHT/Tank.generated.h
+++ ToonTanks/Intermediate/Build/Win64/UnrealEditor/Inc/ToonTanks/UHT/Tank.generated.h
@@ -14,7 +14,7 @@ PRAGMA_DISABLE_DEPRECATION_WARNINGS
 #endif
 #define TOONTANKS_Tank_generated_h
 
-#define FID_ToonTanks_Source_ToonTanks_Tank_h_15_INCLASS_NO_PURE_DECLS \
+#define FID_Users_nicho_OneDrive_portofolio_Nicholas_Cricchiola_Portfolio_ToonTanks_Source_ToonTanks_Tank_h_15_INCLASS_NO_PURE_DECLS \
 private: \
 	static void StaticRegisterNativesATank(); \
 	friend struct Z_Construct_UClass_ATank_Statics; \
@@ -23,7 +23,7 @@ public: \
 	DECLARE_SERIALIZER(ATank)
 
 
-#define FID_ToonTanks_Source_ToonTanks_Tank_h_15_ENHANCED_CONSTRUCTORS \
+#define FID_Users_nicho_OneDrive_portofolio_Nicholas_Cricchiola_Portfolio_ToonTanks_Source_ToonTanks_Tank_h_15_ENHANCED_CONSTRUCTORS \
 private: \
 	/** Private move- and copy-constructors, should never be used */ \
 	ATank(ATank&&); \
@@ -35,12 +35,12 @@ public: \
 	NO_API virtual ~ATank();
 
 
-#define FID_ToonTanks_Source_ToonTanks_Tank_h_12_PROLOG
-#define FID_ToonTanks_Source_ToonTanks_Tank_h_15_GENERATED_BODY \
+#define FID_Users_nicho_OneDrive_portofolio_Nicholas_Cricchiola_Portfolio_ToonTanks_Source_ToonTanks_Tank_h_12_PROLOG
+#define FID_Users_nicho_OneDrive_portofolio_Nicholas_Cricchiola_Portfolio_ToonTanks_Source_ToonTanks_Tank_h_15_GENERATED_BODY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	FID_ToonTanks_Source_ToonTanks_Tank_h_15_INCLASS_NO_PURE_DECLS \
-	FID_ToonTanks_Source_ToonTanks_Tank_h_15_ENHANCED_CONSTRUCTORS \
+	FID_Users_nicho_OneDrive_portofolio_Nicholas_Cricchiola_Portfolio_ToonTanks_Source_ToonTanks_Tank_h_15_INCLASS_NO_PURE_DECLS \
+	FID_Users_nicho_OneDrive_portofolio_Nicholas_Cricchiola_Portfolio_ToonTanks_Source_ToonTanks_Tank_h_15_ENHANCED_CONSTRUCTORS \
 private: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
@@ -48,7 +48,7 @@ PRAGMA_ENABLE_DEPRECATION_WARNINGS
 template<> TOONTANKS_API UClass* StaticClass<class ATank>();
 
 #undef CURRENT_FILE_ID
-#define CURRENT_FILE_ID FID_ToonTanks_Source_ToonTanks_Tank_h
+#define CURRENT_FILE_ID FID_Users_nicho_OneDrive_portofolio_Nicholas_Cricchiola_Portfolio_ToonTanks_Source_ToonTanks_Tank_h
 
 
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
